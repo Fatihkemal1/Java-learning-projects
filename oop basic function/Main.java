1482ff7d1b4eaf8ba39cf73de38d715f5ef50142
Fatih Kemal tomas <92810946+Fatihkemal1@users.noreply.github.com>
2026-01-02 17:46:48 +0300

    Main.java

---
+++ oop basic function/Main.java
@@ -0,0 +1,15 @@
+package çalışmalar;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		
+		human human1 = new human("billie", 24 , 65);
+		human human2= new human("jean", 30, 50);		
+		
+human1.eat();
+human2.sleep();
+
+	}
+
+}
